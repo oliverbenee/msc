94ec94cb377808234568281e4a5df2bc9625f3e3
Oliver <201806928@post.au.dk>
2023-06-05 10:45:53 +0200

    fixed the unique key constraint nonsense

--- db/data/migrations/20230605002141_smhi.js
+++ db/data/migrations/20230605002141_smhi.js
@@ -5,10 +5,10 @@
 exports.up = function(knex) {
     knex.schema.hasTable("smhi").then((exists) => {
     if(!exists) {
-      return knex.schema
+      knex.schema
         .createTable("smhi", table => {
           table.string('device_id')
-	  table.foreign("device_id").references("device_id").inTable("locations").onDelete("CASCADE")
+	        table.foreign("device_id").references("device_id").inTable("locations").onDelete("CASCADE")
           table.timestamp("time").defaultTo(knex.fn.now())
           table.float("t", 5)
           table.float("h", 5)
@@ -19,10 +19,10 @@ exports.up = function(knex) {
           table.float("precip", 5)
           table.float("sun", 5)
           table.float("visibility", 5)
-	  table.unique(["device_id", "time"], {useConstraint: true})
-	  
+	        table.unique(["device_id", "time"], {useConstraint: true})
         })
-        .then(console.log("created table smhi"))
+        .then(() => {knex.raw("ALTER TABLE smhi ADD CONSTRAINT unq_smhi_device_id_time UNIQUE(device_id, time)")}) // knex seems to specifically ignore this one's unique key constraint. this fixes it. 
+        .then(console.log("the SMHI table has been created. Uniquely, it should not return a promise. "))
     }
   })  
 };
